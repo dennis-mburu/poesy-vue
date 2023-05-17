de1adcbb6be537b5c2372bff98437a6015fc960a
Dennnis Mburu Mwaura <dennismburu2063@gmail.com>
2023-05-17 15:29:19 +0300

    UI updated

---
+++ src/components/NewPoemForm.vue
@@ -0,0 +1,31 @@
+<template>
+  <form class="new-poem-form">
+    <input type="text" name="title" placeholder="Title" />
+    <input type="text" name="author" placeholder="Author" />
+    <textarea
+      name="content"
+      rows="10"
+      placeholder="Share your masterpiece here..."
+    ></textarea>
+    <input type="submit" value="Share your masterpiece" />
+  </form>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+    .new-poem-form {
+        border: 1px solid ;
+        padding: 1em;
+        display: flex;
+        flex-direction: column;
+    }
+
+    input, textarea{
+        font-size: 1em;
+        font-family: sans-serif;
+        margin-bottom: 0.5em;
+    }
+</style>
---
+++ src/components/PoemsContainer.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="poems-container">
+    <h1>Poems Container</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis similique cumque cupiditate, fugiat neque quasi qui quae consequuntur porro laboriosam, laborum eos voluptate voluptatum, sit provident. Enim quaerat culpa incidunt!<br><br></p>
+    <p>Harum necessitatibus illo sequi illum sed sit maxime, a voluptate cum. Culpa enim accusamus voluptatum ut! Neque voluptate quos inventore ad error enim! Facere iste provident, hic necessitatibus quibusdam neque!<br><br></p>
+    <p>Soluta nobis, voluptatibus molestias sint exercitationem commodi! Sequi, earum. A atque non vero quas. Ipsam asperiores at reprehenderit consectetur molestiae quam, cumque quidem quasi modi harum? Perferendis quisquam et iure!<br><br></p>
+    <p>Sint sed labore aspernatur sunt, laudantium delectus sapiente? Unde quaerat similique impedit voluptates, nihil possimus aliquid, voluptas sapiente, odio incidunt doloremque eveniet? Labore quidem qui, ipsa esse quam dolores sed?<br><br></p>
+    <p>Ullam perspiciatis impedit recusandae labore. Ullam nesciunt hic alias perferendis, voluptas saepe repudiandae minima! Repudiandae dignissimos ipsam culpa atque ex rem quas eligendi tempore. Labore corporis et adipisci? Maxime, assumenda?<br><br></p>
+    <p>Eum, atque cupiditate magnam quae exercitationem repudiandae ratione pariatur aperiam quis ipsum, ipsam laborum. Fuga, sed, reiciendis tempore possimus hic minus repellendus nihil sint dignissimos eligendi rerum error sunt nam?<br><br></p>
+    <p>Officia aperiam accusantium inventore recusandae fugit! Laboriosam, sapiente quo maiores alias earum necessitatibus est cupiditate quibusdam illo tenetur vitae perspiciatis. Reprehenderit dignissimos recusandae rerum officia repudiandae esse eos asperiores nesciunt!<br><br></p>
+    <p>Laborum magni placeat deserunt delectus neque. Fugiat vel accusamus ullam accusantium omnis autem rerum iure, molestias eius corrupti perferendis ipsum cum consequatur eveniet pariatur aspernatur voluptatibus debitis voluptas magnam enim.<br><br></p>
+    <p>Harum quis ipsa aliquid quos nisi corporis. Exercitationem iure minus ullam natus fugit dolorem perferendis voluptatem labore nostrum, eveniet commodi vero modi earum atque ab? Blanditiis dicta aliquam animi veniam?<br><br></p>
+    <p>Voluptas odit aut inventore libero. Reiciendis minus at numquam enim, facere repudiandae rem velit nihil? Eaque incidunt ipsum voluptate quisquam nostrum. Voluptatum vel fugit fuga maxime, vitae porro minima provident.<br><br></p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+    .poems-container {
+        max-width: 30em;
+        margin: auto;
+    }
+
+</style>
