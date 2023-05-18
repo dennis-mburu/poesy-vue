f489480f04e49a1ef4ef282e26a9eef05eb5583d
Dennnis Mburu Mwaura <dennismburu2063@gmail.com>
2023-05-18 11:26:38 +0300

    changes adding and deleting poem to use async await syntax

--- src/components/NewPoemForm.vue
+++ src/components/NewPoemForm.vue
@@ -1,7 +1,7 @@
 <template>
   <form class="new-poem-form" @submit="handleSubmit">
-    <input type="text" name="title" placeholder="Title" v-model="title"/>
-    <input type="text" name="author" placeholder="Author" v-model="author"/>
+    <input type="text" name="title" placeholder="Title" v-model="title" />
+    <input type="text" name="author" placeholder="Author" v-model="author" />
     <textarea
       name="content"
       rows="10"
@@ -15,55 +15,67 @@
 
 <script>
 export default {
-  data(){
+  data() {
     return {
       title: "",
-      author: '',
+      author: "",
       content: "",
-    }
+    };
   },
   methods: {
-    handleSubmit(e){
-      e.preventDefault()
+    async handleSubmit(e) {
+      e.preventDefault();
 
       const newPoem = {
         title: this.title,
         author: this.author,
-        content: this.content
+        content: this.content,
+      };
+
+      // fetch('http://localhost:3002/poems', {
+      //   method: 'POST',
+      //   headers: {
+      //     'Content-Type': 'application/json'
+      //   },
+      //   body: JSON.stringify(newPoem)
+      // })
+      // .then(res => res.json())
+      // .then(data => this.$emit('add-newPoem', data))
+
+      // Trying async await syntax
+      try {
+        const res = await fetch("http://localhost:3002/poems", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(newPoem),
+        });
+        // console.log(res)
+        const poem = await res.json();
+        this.$emit("add-newPoem", poem);
+        (this.title = ""), (this.author = ""), (this.content = "");
+      } catch (error) {
+        // console.log(error);
+        alert(error);
       }
-
-      fetch('http://localhost:3002/poems', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(newPoem)
-      })
-      .then(res => res.json())
-      .then(data => this.$emit('add-newPoem', data))
-
-
-      // console.log(newTask)
-
-      this.title = "",
-      this.author = '',
-      this.content = ""
     },
-  }
+  },
 };
 </script>
 
 <style scoped>
-    .new-poem-form {
-        border: 1px solid ;
-        padding: 1em;
-        display: flex;
-        flex-direction: column;
-    }
+.new-poem-form {
+  border: 1px solid;
+  padding: 1em;
+  display: flex;
+  flex-direction: column;
+}
 
-    input, textarea{
-        font-size: 1em;
-        font-family: sans-serif;
-        margin-bottom: 0.5em;
-    }
+input,
+textarea {
+  font-size: 1em;
+  font-family: sans-serif;
+  margin-bottom: 0.5em;
+}
 </style>
--- src/components/Poem.vue
+++ src/components/Poem.vue
@@ -16,23 +16,36 @@
 export default {
   props: ["poem"],
   methods: {
-    handleDelete(id) {
+    async handleDelete(id) {
       // this can work too without passing the poem.id on
       // the click event, just leaving it as handleDelete
       // console.log(this.poem.id)
 
-      fetch(`http://localhost:3002/poems/${id}`, {
-        method: "DELETE",
-      })
-        .then((res) => {
-          //   console.log(res);
-          res.ok
-            ? this.$emit("delete-poem", id)
-            : alert("Error: " + res.statusText);
-        })
-        .catch((err) => {
-          alert(err);
+      //   fetch(`http://localhost:3002/poems/${id}`, {
+      //     method: "DELETE",
+      //   })
+      //     .then((res) => {
+      //       //   console.log(res);
+      //   res.ok
+      //     ? this.$emit("delete-poem", id)
+      //     : alert("Error: " + res.statusText);
+      //     })
+      //     .catch((err) => {
+      //       alert(err);
+      //     });
+
+      // Trying async await syntax
+
+      try {
+        const res = await fetch(`http://localhost:3002/poems/${id}`, {
+          method: "DELETE",
         });
+        res.ok
+          ? this.$emit("delete-poem", id)
+          : alert("Error: " + res.statusText);
+      } catch (error) {
+        alert(error);
+      }
     },
   },
 };
